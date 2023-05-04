4931e2e4ec71b44d5c465f5737469d5c2a614e12
JBecconsall <joshuabecconsall05@gmail.com>
2023-05-04 16:39:33 +0100

    setup project

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const {Client, IntentsBitField } = require('discord.js');
 const CH = require('cmd-handler');
+const path = require('path');
 require('dotenv/config')
 
 const client = new Client({
@@ -10,6 +11,11 @@ const client = new Client({
 
 client.on('ready', () => {
     console.log('The bot is online!')
+
+    new CH({
+        client,
+        commandsDir: path.join(__dirname, 'commands')
+    })
 
 });
 
