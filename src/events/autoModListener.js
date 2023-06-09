e1384688de1ba4f56c6b79c50e44a3713ec98851
JBecconsall <joshuabecconsall05@gmail.com>
2023-06-10 00:51:20 +0100

    started automod

---
+++ src/events/autoModListener.js
@@ -0,0 +1,67 @@
+const { EmbedBuilder, MessagePayload } = require("discord.js")
+const { client } = require("../index");
+
+
+module.exports = async(interaction, message) => {
+    
+    client.on('messageCreate', async (message) => {
+
+        
+    if(message.content.includes('discord.gg/')) {
+
+        if (!message.member.roles.cache.has('1116876087989903411')) {
+
+        try {
+            message.delete().then(message.member.timeout(300000, 'Sending invite links'))
+        
+        } catch (err) {
+            console.log(err)
+        }
+        const autoModEmbed = new EmbedBuilder()
+        .setTitle(`⚠️ Auto Moderation`)
+        .addFields(
+            {name: 'Action', value:  "`" + `Timeout (AutoMod)` + "`"},
+            {name: 'Details', value: "`" + 'User:' + "` " + `<@${message.author.id}>` + "`" + `(${message.author.id})` + "`\n"  + "`" + 'Time' + "` " + "5m\n" + "`" + "Reason:" + "` " + "Posted an invite link in " + `<#${message.channel.id}> ` + "`" + "(" + `${message.content})` + "`"}
+        )
+        .setTimestamp()
+        .setColor('Red')
+
+        let automodChannel = message.guild.channels.cache.find(c => c.id === '1114178682295164959')
+
+        let logMsg = MessagePayload.create(automodChannel, {
+            embeds: [autoModEmbed]
+        })
+
+        await automodChannel.send(logMsg)
+
+        const dmEmbed = new EmbedBuilder()
+        .setTitle(`⚠️ Timed Out`)
+        .setDescription(`You were timed out in ${message.guild.name}`)
+        .addFields(
+            {name: 'Details', value: "`" + 'Time' + "` " + "5m\n" + "`" + "Reason:" + "` " + "Posted an invite link in " + `<#${message.channel.id}> ` + "`" + "(" + `${message.content})` + "`"}
+        )
+        .setTimestamp()
+        .setColor('Red')
+
+        let dmMsg = MessagePayload.create(automodChannel, {
+            embeds: [dmEmbed]
+        })
+
+
+        try {
+            message.author.send(dmMsg)
+        } catch (err) {
+            console.log(err)
+        }
+
+        } else {
+
+            console.log('Bypass')
+
+        
+        
+    }
+}
+    
+    })
+}
