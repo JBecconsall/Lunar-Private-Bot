1af749338cc352333ec3c11f0f31d32cfcc790b9
JBecconsall <joshuabecconsall05@gmail.com>
2023-06-10 01:06:51 +0100

    automod update
    Need to make the automod system better, need to make a proper way for it to catch racism etc rather than inputting every type of racist word.

--- src/events/autoModListener.js
+++ src/events/autoModListener.js
@@ -1,67 +1,149 @@
-const { EmbedBuilder, MessagePayload } = require("discord.js")
-const { client } = require("../index");
+const {
+    EmbedBuilder,
+    MessagePayload
+} = require("discord.js")
+const {
+    client
+} = require("../index");
 
 
-module.exports = async(interaction, message) => {
-    
+module.exports = async (interaction, message) => {
+
     client.on('messageCreate', async (message) => {
 
-        
-    if(message.content.includes('discord.gg/')) {
 
-        if (!message.member.roles.cache.has('1116876087989903411')) {
+        if (message.content.includes('discord.gg/')) {
 
-        try {
-            message.delete().then(message.member.timeout(300000, 'Sending invite links'))
-        
-        } catch (err) {
-            console.log(err)
+            if (!message.member.roles.cache.has('1116876087989903411')) {
+
+                try {
+                    message.delete().then(message.member.timeout(300000, 'Sending invite links'))
+
+                } catch (err) {
+                    console.log(err)
+                }
+                const autoModEmbed = new EmbedBuilder()
+                    .setTitle(`⚠️ Auto Moderation`)
+                    .addFields({
+                        name: 'Action',
+                        value: "`" + `Timeout (AutoMod)` + "`"
+                    }, {
+                        name: 'Details',
+                        value: "`" + 'User:' + "` " + `<@${message.author.id}>` + "`" + `(${message.author.id})` + "`\n" + "`" + 'Time' + "` " + "5m\n" + "`" + "Reason:" + "` " + "Posted an invite link in " + `<#${message.channel.id}> ` + "`" + "(" + `${message.content})` + "`"
+                    })
+                    .setTimestamp()
+                    .setColor('Red')
+
+                let automodChannel = message.guild.channels.cache.find(c => c.id === '1114178682295164959')
+
+                let logMsg = MessagePayload.create(automodChannel, {
+                    embeds: [autoModEmbed]
+                })
+
+                await automodChannel.send(logMsg)
+
+                const dmEmbed = new EmbedBuilder()
+                    .setTitle(`⚠️ Moderation Alert`)
+                    .setDescription(`You were timed out in ${message.guild.name}`)
+                    .addFields({
+                        name: 'Details',
+                        value: "`" + 'Time' + "` " + "5m\n" + "`" + "Reason:" + "` " + "Posted an invite link in " + `<#${message.channel.id}> ` + "`" + "(" + `${message.content})` + "`"
+                    })
+                    .setTimestamp()
+                    .setColor('Red')
+
+                let dmMsg = MessagePayload.create(automodChannel, {
+                    embeds: [dmEmbed]
+                })
+
+
+                try {
+                    message.author.send(dmMsg)
+                } catch (err) {
+                    console.log(err)
+                }
+
+            } else {
+
+                console.log('Bypass')
+
+
+
+            }
         }
-        const autoModEmbed = new EmbedBuilder()
-        .setTitle(`⚠️ Auto Moderation`)
-        .addFields(
-            {name: 'Action', value:  "`" + `Timeout (AutoMod)` + "`"},
-            {name: 'Details', value: "`" + 'User:' + "` " + `<@${message.author.id}>` + "`" + `(${message.author.id})` + "`\n"  + "`" + 'Time' + "` " + "5m\n" + "`" + "Reason:" + "` " + "Posted an invite link in " + `<#${message.channel.id}> ` + "`" + "(" + `${message.content})` + "`"}
-        )
-        .setTimestamp()
-        .setColor('Red')
 
-        let automodChannel = message.guild.channels.cache.find(c => c.id === '1114178682295164959')
+        if (message.content.includes('nigger') || message.content.includes('Nigger')) {
 
-        let logMsg = MessagePayload.create(automodChannel, {
-            embeds: [autoModEmbed]
-        })
+            if (!message.member.roles.cache.has('1116876087989903411')) {
 
-        await automodChannel.send(logMsg)
+                let automodChannel = message.guild.channels.cache.find(c => c.id === '1114178682295164959')
 
-        const dmEmbed = new EmbedBuilder()
-        .setTitle(`⚠️ Timed Out`)
-        .setDescription(`You were timed out in ${message.guild.name}`)
-        .addFields(
-            {name: 'Details', value: "`" + 'Time' + "` " + "5m\n" + "`" + "Reason:" + "` " + "Posted an invite link in " + `<#${message.channel.id}> ` + "`" + "(" + `${message.content})` + "`"}
-        )
-        .setTimestamp()
-        .setColor('Red')
-
-        let dmMsg = MessagePayload.create(automodChannel, {
-            embeds: [dmEmbed]
-        })
+                const dmEmbed = new EmbedBuilder()
+                    .setTitle(`⚠️ Moderation Alert`)
+                    .setDescription(`You were banned from ${message.guild.name}`)
+                    .addFields({
+                        name: 'Details',
+                        value: "`" + 'Time' + "` " + "Permanent\n" + "`" + "Reason:" + "` " + "Posted a banned word in " + `<#${message.channel.id}> ` + "`" + "(" + `${message.content})` + "`"
+                    })
+                    .setTimestamp()
+                    .setColor('Red')
 
 
-        try {
-            message.author.send(dmMsg)
-        } catch (err) {
-            console.log(err)
-        }
+                let dmMsg = MessagePayload.create(automodChannel, {
+                    embeds: [dmEmbed]
+                })
 
-        } else {
 
-            console.log('Bypass')
+                
+                try {
+                    message.author.send(dmMsg)
+                } catch (err) {
+                    console.log(err)
+                }
 
-        
-        
-    }
-}
+
+                setTimeout(() => {
+                    try {
+                        message.delete().then(
+                            message.guild.bans.create(message.member, {reason: 'Racism'})
+                        )
     
+                    } catch (err) {
+                        console.log(err)
+                    }
+
+                }, 5000)
+                
+                const autoModEmbed = new EmbedBuilder()
+                    .setTitle(`⚠️ Auto Moderation`)
+                    .addFields({
+                        name: 'Action',
+                        value: "`" + `Ban (AutoMod)` + "`"
+                    }, {
+                        name: 'Details',
+                        value: "`" + 'User:' + "` " + `<@${message.author.id}>` + "`" + `(${message.author.id})` + "`\n" + "`" + 'Time' + "` " + "Permanent\n" + "`" + "Reason:" + "` " + "Posted a banned word in " + `<#${message.channel.id}> ` + "`" + "(" + `${message.content})` + "`"
+                    })
+                    .setTimestamp()
+                    .setColor('Red')
+
+    
+
+                let logMsg = MessagePayload.create(automodChannel, {
+                    embeds: [autoModEmbed]
+                })
+
+                await automodChannel.send(logMsg)
+
+                
+            
+            } else {
+
+                console.log('Bypass')
+
+
+
+            }
+        }
+
     })
 }
